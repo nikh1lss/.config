83a8cf1e8edb58de28e0f6759409774f76818ea6
Nikhil <nikhil.singh1@student.unimelb.edu.au>
2025-12-19 21:04:04 +1100

    init

---
+++ config/nvim/lua/autocmds.lua
@@ -0,0 +1,12 @@
+-- File for autocommands
+
+
+-- Autocommand to standardize tabs for all filetypes
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+    end,
+})
