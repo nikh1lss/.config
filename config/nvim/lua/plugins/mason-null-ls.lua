0f0807c643fa1f3fbb5e1cc9d307eac5d948762c
Nikhil <nikhil.singh1@student.unimelb.edu.au>
2025-12-29 01:19:15 +1100

    clean nvim

--- config/nvim/lua/plugins/mason-null-ls.lua
+++
@@ -1,9 +0,0 @@
----@type LazySpec
-return {
-  "jay-babu/mason-null-ls.nvim",
-  opts = {
-    handlers = {
-      checkstyle = function() end, -- disable auto-setup, we configure it manually in none-ls.lua
-    },
-  },
-}
