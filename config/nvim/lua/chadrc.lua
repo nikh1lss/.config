79afdaa5e4a5d4df7a79a11324b936c65e83e955
Nikhil <nikhil.singh1@student.unimelb.edu.au>
2025-12-30 21:03:58 +1100

    why hide behind a plugin?

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -1,24 +1,127 @@
 -- This file needs to have same structure as nvconfig.lua 
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 -- Please read that file to know all available options :( 
-
 ---@type ChadrcConfig
-local M = {}
+local options = {
 
-M.base46 = {
-	theme = "onedark",
+  base46 = {
+    theme = "tokyodark", -- default theme
+    hl_add = {},
+    hl_override = {
+      Comment = { italic = true },
+      ["@comment"] = { italic = true },
+    },
+    integrations = {},
+    changed_themes = {},
+    transparency = true,
+    theme_toggle = { "onedark", "one_light" },
+  },
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  ui = {
+    cmp = {
+      icons_left = false, -- only for non-atom styles!
+      style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+      abbr_maxwidth = 60,
+      -- for tailwind, css lsp etc
+      format_colors = { lsp = true, icon = "󱓻" },
+    },
+
+    telescope = { style = "borderless" }, -- borderless / bordered
+
+    statusline = {
+      enabled = true,
+      theme = "default", -- default/vscode/vscode_colored/minimal
+      -- default/round/block/arrow separators work only for default statusline theme
+      -- round and block will work for minimal theme only
+      separator_style = "default",
+      order = nil,
+      modules = nil,
+    },
+
+    -- lazyload it when there are 1+ buffers
+    tabufline = {
+      enabled = true,
+      lazyload = true,
+      order = { "treeOffset", "buffers", "tabs", "btns" },
+      modules = nil, -- nice!
+      bufwidth = 21,
+    },
+  },
+
+  nvdash = {
+    load_on_startup = true,
+    header = {
+      "                      ",
+      "  ▄▄         ▄ ▄▄▄▄▄▄▄",
+      "▄▀███▄     ▄██ █████▀ ",
+      "██▄▀███▄   ███        ",
+      "███  ▀███▄ ███        ",
+      "███    ▀██ ███        ",
+      "███      ▀ ███        ",
+      "▀██ █████▄▀█▀▄██████▄ ",
+      "  ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀",
+      "                      ",
+      "  Powered By Neovim  ",
+      "                      ",
+    },
+
+    buttons = {
+      { txt = "  Find File", keys = "f", cmd = "Telescope find_files" },
+      { txt = "  Recent Files", keys = "o", cmd = "Telescope oldfiles" },
+      { txt = "󰈭  Find Word", keys = "w", cmd = "Telescope live_grep" },
+      -- { txt = "󱥚  Themes", keys = "t", cmd = ":lua require('nvchad.themes').open()" },
+      -- { txt = "  Mappings", keys = "m", cmd = "NvCheatsheet" },
+      { txt = "   Last Session", keys = "s", cmd = "" },
+      { txt = "   Bookmarks", keys = "'", cmd = "" },
+
+      { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+
+      {
+        txt = function()
+          local stats = require("lazy").stats()
+          local ms = math.floor(stats.startuptime) .. " ms"
+          return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+        end,
+        hl = "NvDashFooter",
+        no_gap = true,
+        content = "fit",
+      },
+
+      { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    },
+  },
+
+  term = {
+    startinsert = true,
+    base46_colors = true,
+    winopts = { number = false, relativenumber = true },
+    sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+    float = {
+      relative = "editor",
+      row = 0.3,
+      col = 0.25,
+      width = 0.5,
+      height = 0.4,
+      border = "single",
+    },
+  },
+
+  lsp = { signature = true },
+
+  cheatsheet = {
+    theme = "grid", -- simple/grid
+    excluded_groups = { "terminal (t)", "autopairs", "Nvim", "Opens" }, -- can add group name or with mode
+  },
+
+  mason = { pkgs = {}, skip = {} },
+
+  colorify = {
+    enabled = true,
+    mode = "virtual", -- fg, bg, virtual
+    virt_text = "󱓻 ",
+    highlight = { hex = true, lspvars = true },
+  },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
-
-return M
+local status, chadrc = pcall(require, "chadrc")
+return vim.tbl_deep_extend("force", options, status and chadrc or {})
--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,6 +1,90 @@
-require "nvchad.options"
-
--- add yours here!
-
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local opt = vim.opt
+-- local o = vim.o
+local g = vim.g
+
+-------------------------------------- options ------------------------------------------
+
+opt.guicursor = ""
+opt.spell = false
+opt.signcolumn = "yes"
+opt.wrap = false
+opt.showmode = true
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.colorcolumn = "100"
+opt.ignorecase = true
+opt.smartcase = true
+opt.cursorline = true
+opt.termguicolors = true
+opt.conceallevel = 0
+
+-- Splits
+opt.splitbelow = true
+opt.splitright = true
+opt.splitkeep = "screen"
+
+-- Editing
+opt.clipboard = "unnamedplus"
+opt.mouse = "a"
+opt.undofile = true
+opt.timeoutlen = 400
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append "<>[]hl"
+
+-- Performance
+-- interval for writing swap file to disk, also used by gitsigns
+opt.updatetime = 250
+
+-- Disable providers
+-- disable some default providers
+g.loaded_node_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Line Numbers
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 4
+opt.ruler = false
+
+opt.laststatus = 3
+
+opt.cursorline = true
+opt.cursorlineopt = "number"
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+
+opt.fillchars = { eob = " " }
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Numbers
+opt.number = true
+opt.numberwidth = 4 -- initially 2
+opt.ruler = false
+
+-- disable nvim intro
+opt.shortmess:append "sI"
+
+opt.signcolumn = "yes"
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.fn.has "win32" ~= 0
+local sep = is_windows and "\\" or "/"
+local delim = is_windows and ";" or ":"
+vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
