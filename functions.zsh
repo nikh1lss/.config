cd1f1f2af088bc19ce10daf6b9ae954b6086f8f1
Nikhil <nikhil.singh1@student.unimelb.edu.au>
2026-01-23 15:39:08 +1100

    feat(es): reindexing and service check functions

--- functions.zsh
+++ functions.zsh
@@ -1,9 +1,37 @@
 # zshrc functions
 
+# es.exe blocks while indexing, therefore exit code of 0 means indexing is complete
+_es_wait_for_index() {
+  es.exe -max-results 1 "" &>/dev/null
+}
+
+# function for reindexing es.exe db
+function esreindex() {
+  Everything.exe -reindex -close
+  echo "Reindexing..."
+  _es_wait_for_index
+  echo "Done"
+}
+
+# Ensure services are running prior to function usage
+_ensure_services() {
+  if ! es.exe -max-results 1 >/dev/null 2>&1; then
+    echo "Waiting for es.exe service"
+    sleep 2
+    if ! es.exe -max-results 1 >/dev/null 2>&1; then
+      echo "es.exe service not available"
+      return 1
+    fi
+  fi
+  return 0
+}
+
 # Function to find directory and cd or for a file, extract its parent directory with dirname and cd
 # old fzf config: fzf --preview 'bat --color=always {} 2>/dev/null || cat {}'
 # new fzf config requires bat in replacement of cat, install with brew install bat or apt install bat
 function lcd() {
+  _ensure_services || return 1
+
   local file
   file=$(es.exe -i "$1" 2>/dev/null | tr -d '\r' | fzf \
     --preview 'bat --style=numbers --color=always --line-range :500 {} 2>/dev/null || ls -lah --color=always {} 2>/dev/null || echo "¯\_(ツ)_/¯"' \
@@ -35,6 +63,8 @@ function lcd() {
 # Find file and edit it - does not cd to its directory, we stay in our current directory
 # If we led to a directory and not a file, it will open that directory in nvim which is fine
 function led() {
+  _ensure_services || return 1
+
   local file
   file=$(es.exe -i "$1" 2>/dev/null | tr -d '\r' | fzf \
     --preview 'bat --style=numbers --color=always --line-range :500 {} 2>/dev/null || ls -lah --color=always {} 2>/dev/null || echo "¯\_(ツ)_/¯"' \
