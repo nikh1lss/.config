d135a2153dff1862b6bf9648cb400ada13a89db8
Nikhil <nikhil.singh1@student.unimelb.edu.au>
2026-01-31 02:03:23 +1100

    oil

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -492,4 +492,49 @@ return {
       vim.keymap.set("n", "<leader>Je", springboot.generate_enum, { desc = "Generate Enum" })
     end,
   },
+
+  -- oil
+  {
+    "stevearc/oil.nvim",
+    cmd = "Oil",
+    keys = {
+      { "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
+      { "<leader>o", "<cmd>Oil --float<CR>", desc = "Open Oil (float)" },
+    },
+    opts = {
+      default_file_explorer = true,
+      columns = {
+        "icon",
+        "permissions",
+        "size",
+        "mtime",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+      float = {
+        padding = 2,
+        max_width = 90,
+        max_height = 30,
+      },
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-x>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["q"] = "actions.close",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
 }
