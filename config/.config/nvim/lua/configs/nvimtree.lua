5aaf9f8d1a2388c233d4bef7a481c1cde0963f5f
Nikhil <nsingh12004@gmail.com>
2026-09-26 22:19:21 +1000

    fix(nvimtree): disable experimental session_restore_nvim

--- config/.config/nvim/lua/configs/nvimtree.lua
+++ config/.config/nvim/lua/configs/nvimtree.lua
@@ -3,6 +3,8 @@ return {
   disable_netrw = true,
   hijack_cursor = true,
   sync_root_with_cwd = true,
+  -- needs the SessionWritePre event, which only newer 0.13 nightlies have
+  experimental = { session_restore_nvim = false },
   update_focused_file = {
     enable = true,
     update_root = false,
